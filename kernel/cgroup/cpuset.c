e0829c213ab23b2ac0d7503c58b815aec9d5e259
0ctobot <return.of.octobot@gmail.com>
2019-11-05 04:49:26 -0500

    SchedTune/Cpuset Assist: Improve node tampering protection
    Subsequent to dd73a14 ("exec: Add node tampering blacklist function")
    
    Utilizing the task_is_booster function should prove to be much more
    comprehensive and reliable in protecting our in-kernel
    configurations from external interference rather than simply
    checking comm for init.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -57,6 +57,7 @@
 #include <linux/backing-dev.h>
 #include <linux/sort.h>
 #include <linux/oom.h>
+#include <linux/binfmts.h>
 
 #include <linux/uaccess.h>
 #include <linux/atomic.h>
@@ -1802,7 +1803,7 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
 
-	if (!strcmp(current->comm, "init")) {
+	if (task_is_booster(current)) {
 		for (i = 0; i < ARRAY_SIZE(cs_targets); i++) {
 			struct cs_target tgt = cs_targets[i];
 
